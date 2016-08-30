ac676901c04e6d1d175fb8465ebc24032950ff8f
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-08-30 12:06:28 +0800

    Commit.

--- src/bin.py
+++ src/bin.py
@@ -1,5 +1,6 @@
 import urllib
 import re
+from email.parser import parser
 
 src = urllib.urlopen('http://butter:fly@www.pythonchallenge.com/pc/hex/bin.html').read()
 text = re.compile('<!--((?:[^-]+|-[^-]|--[^>])*)-->').findall(src)[0]
