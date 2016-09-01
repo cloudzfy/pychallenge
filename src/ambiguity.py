21f454345292c6e105bc4f3ec0daa3f5b3e2b194
Youzheng Chen <cloudzfy@users.noreply.github.com>
2016-09-01 16:47:33 +0800

    Commit.

--- src/ambiguity.py
+++ src/ambiguity.py
@@ -1,7 +1,48 @@
 import Image
 import urllib
 import StringIO
+import zipfile
 
 src = urllib.urlopen('http://butter:fly@www.pythonchallenge.com/pc/hex/maze.png').read()
 im = Image.open(StringIO.StringIO(src))
 
+black = (0, 0, 0, 255)
+white = (255, 255, 255, 255)
+
+for x in range(0, im.size[0]):
+	if im.getpixel((x, 0)) == black:
+		entrance = (x, 0)
+	if im.getpixel((x, im.size[1] - 1)) == black:
+		exit = (x, im.size[1] - 1)
+
+move = {}
+move[exit] = None
+queue = [exit]
+steps = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+def checkBoundary(pos):
+	return pos[0] >= 0 and pos[0] < im.size[0] and pos[1] >= 0 and pos[1] < im.size[1]
+
+while len(queue) != 0:
+	pos = queue.pop(0)
+	if pos == entrance:
+		break
+	for s in steps:
+		tmp = tuple(map(lambda x, y: x + y, pos, s))
+		if checkBoundary(tmp) and not move.has_key(tmp) and im.getpixel(tmp) != white:
+			queue.append(tmp)
+			move[tmp] = pos
+
+path = []
+pos = entrance
+while pos != exit:
+	path.append(chr(im.getpixel(pos)[0]))
+	pos = move[pos]
+
+z = zipfile.ZipFile(StringIO.StringIO(''.join(path[1::2])))
+Image.open(StringIO.StringIO(z.read('maze.jpg'))).show()
+
+file = open('mybroken.zip', 'wb')
+file.write(z.read('mybroken.zip'))
+file.close()
+print 'mybroken.zip'
--- src/bin.py
+++ src/bin.py
@@ -24,3 +24,25 @@ wav2.writeframes(wav1.readframes(wav1.getnframes())[0::2])
 wav1.close()
 wav2.close()
 print 'idiot.wav'
+
+# Email received from Leopold Mozart:
+#
+# From: "Leopold Mozart" <leopold.moz@pythonchallenge.com>
+# Date: Thu, 1 Sep 2016 01:39:31 -0700
+# Message-ID: <CALLh7wynAe1OncB50MLBVeTLwG7HT8=-BRKx-gg1zAga2iZPeA@mail.gmail.com>
+# Subject: Re: my broken zip Re: sorry
+# MIME-Version: 1.0
+# Content-Type: text/plain; charset=UTF-8
+# Content-Transfer-Encoding: 7bit
+# Content-Disposition: inline
+# Precedence: bulk
+# X-Autoreply: yes
+# Auto-Submitted: auto-replied
+# 
+# Never mind that.
+# 
+# Have you found my broken zip?
+# 
+# md5: bbb8b499a0eef99b52c7f13f4e78c24b
+# 
+# Can you believe what one mistake can lead to?
